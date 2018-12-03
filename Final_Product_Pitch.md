4570a6be7a83b23011c4e989a5701c6159fd2737
shaynweidner <shaynweidner@gmail.com>
2018-12-02 20:44:24 -0600

    First commit for DDP repo

---
+++ Final_Product_Pitch.md
@@ -0,0 +1,61 @@
+Final Product Pitch
+========================================================
+author: Shayn Weidner
+date: 12/2/18
+autosize: true
+
+
+This project was created to satisfy the final project requirements of the Developing Data Products course, part of the Data Science Specialization by JHU on Coursera.
+
+
+App Purpose
+========================================================
+
+There are many times when it is useful to simulate data, and often we would like columns of that data to be correlated.  This app will generate bivariate normal variables with the desired correlations.  This can be done using the mvrnorm() function from the MASS library, but the app facilitates doing so without editing code (and without needing to perform other nuisance tasks, such as converting correlations to covariances, which the mvrnorm function is expecting).  The app then goes further by allowing you to interactively view the generated bivariate normal data's density.  In the future (read: not implemented yet) we could allow the user to export the data, to csv (for instance).  The next slides show how one would manually create and plot the data, while the last slide gives the links to the app (and the github page for the project) and describes how to use it.
+
+Multivariate Normal, via code
+========================================================
+
+
+```r
+library(MASS)
+MyData <- as.data.frame(mvrnorm(n=1000 #sample size
+                 ,mu=c(2,3) #means
+                 ,Sigma = matrix(c(1,0,0,1) #cov matrix
+                                 ,nrow=2)
+                 ))
+names(MyData)<-c("Norm1","Norm2")
+head(MyData)
+```
+
+```
+     Norm1    Norm2
+1 1.447782 1.469845
+2 2.029031 3.259330
+3 1.391564 4.360358
+4 3.144766 5.878543
+5 1.974510 3.616033
+6 1.415213 3.139162
+```
+
+Slide With Plot
+========================================================
+
+
+```r
+library(dplyr)
+library(plotly)
+kd <- with(MyData, MASS::kde2d(Norm1, Norm2, n = round(5*log(nrow(MyData)))))
+p <- plot_ly(x = kd$x, y = kd$y, z = kd$z) %>% add_surface()
+htmlwidgets::saveWidget(as.widget(p), file = "plot.html") # necessary to get functionality in presentation
+```
+<iframe src="plot.html" style="position:absolute;height:100%;width:100%"></iframe>
+
+Links
+========================================================
+
+Below are the links to the App and the github repo for the entire project (including this presentation).  The app includes basic instructions but there really aren't any non-basic instructions.  The app is defaulted to certain parameters, and you change them as necessary to generate data from your desired bivariate normal distribution.  That is, you update how big of a sample you'd like, what the means and variances of the normal distributions to be, and what you want the correlation between the two normal distributions to be (and set a seed).  And that's it.
+
+* [App](https://shaynweidner.shinyapps.io/dataproductsfinalproject/)
+
+* [Github](https://github.com/shaynweidner/DataScienceProductsFinal)
---
+++ app.R
@@ -0,0 +1,125 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(MASS)
+library(plotly)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Correlated Joint Normal Distributions"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+     
+       sidebarPanel(
+         verticalLayout(
+         numericInput(
+           "samplesize",
+           "Sample Size",
+           value = 1000,
+           min=10,
+           max=10000,
+           step=10
+         ),
+         numericInput(
+           "Norm1Mean",
+           "Mean of 1st Distribution",
+           value = 0,
+           step=1
+         ),
+         numericInput(
+           "Norm2Mean",
+           "Mean of 2nd Distribution",
+           value = 0,
+           step=1
+         ),
+         numericInput(
+           "Norm1Var",
+           "Variance of 1st Distribution",
+           value = 1,
+           min=0,
+           step=1
+         ),
+         numericInput(
+           "Norm2Var",
+           "Variance of 2nd Distribution",
+           value = 1,
+           min=0,
+           step=1
+         ),
+         numericInput(
+           "MyCor",
+           "Desired Correlation",
+           value = .5,
+           min=-1,
+           max=1,
+           step=.01
+         ),
+         numericInput(
+           "MySeed",
+           "Random Seed",
+           value = 1234,
+           min=0,
+           step=1
+         ),
+         actionButton(
+           inputId = "Submit",
+           label = "Submit"
+         )
+       ,
+       HTML(
+         "<h3>Basic Instructions</h3><h4>1.Adjust any inputs above from their default values, as necessary<br>2.Hit 'Submit'<br>Interactive plot will appear to the right, after processing has completed (you may need to hover your mouse over it before it shows)<br><br>For more information, go to the app pitch <a href='placeholder.html'>here</a></h4>"
+         )
+      )),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+        plotlyOutput("jointPlot"),
+        textOutput("ActualCor")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  SampleSize = eventReactive(input$Submit, {input$samplesize})
+  Norm1Mean = eventReactive(input$Submit, {input$Norm1Mean})
+  Norm2Mean = eventReactive(input$Submit, {input$Norm2Mean})
+  Norm1Var = eventReactive(input$Submit, {input$Norm1Var})
+  Norm2Var = eventReactive(input$Submit, {input$Norm2Var})
+  MyCor = eventReactive(input$Submit, {input$MyCor})
+  MySeed = eventReactive(input$Submit, {input$MySeed})
+  MyCov = eventReactive(input$Submit, {MyCor() * sqrt(Norm1Var()) * sqrt(Norm2Var())})
+  
+  eventReactive(input$Submit, {set.seed(MySeed())})
+  MyData = eventReactive(input$Submit, {as.data.frame(mvrnorm(n=SampleSize(),mu=c(Norm1Mean(),Norm2Mean()),Sigma = matrix(c(Norm1Var(),MyCov(),MyCov(),Norm2Var()),nrow=2)))})
+  kd <- eventReactive(input$Submit, {kde2d((MyData())[,1], (MyData())[,2], n = round(5*log(SampleSize())))})
+  #names(MyData)<-c("Norm1","Norm2")
+  SampleCor <- eventReactive(input$Submit, {round(cor((MyData())[,1],(MyData())[,2]),3)})
+  
+  
+  output$jointPlot <- renderPlotly({
+    plot_ly(x = (kd())$x, y = (kd())$y, z = (kd())$z) %>% add_surface()
+    }
+  )
+  
+  output$ActualCor <- renderText({
+    paste0("Actual sample correlation = ",SampleCor())
+  })
+
+  
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
